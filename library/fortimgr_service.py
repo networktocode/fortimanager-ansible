789752c58df5c45f4cc068fc1a9a02a658cb066f
Jacob McGill <jacob@networktocode.com>
2017-07-25 13:37:13 -0400

    Sync changes to base code for get_diff_mappings

--- library/fortimgr_service.py
+++ library/fortimgr_service.py
@@ -963,8 +963,9 @@ class FortiManager(object):
         if not present:
             config = proposed
             config["dynamic_mapping"][0]["_scope"] = [proposed_scope]
-            for mapping in existing_map:
-                config["dynamic_mapping"].append(dict(_scope=mapping["_scope"]))
+            if existing_map:
+                for mapping in existing_map:
+                    config["dynamic_mapping"].append(dict(_scope=mapping["_scope"]))
 
         return config
 
@@ -1075,7 +1076,7 @@ class FortiManager(object):
                     # keep unrelated mapping in diff so that diff can be used to update FortiManager
                     config["dynamic_mapping"].append(dict(_scope=mapping["_scope"]))
 
-        # set config to dict with name only if mapping does not exist representing no change
+        # set config to empty dict if mapping was not found, representing no change
         if not present:
             config = {}
 
